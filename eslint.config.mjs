41a2038bdeea22595157d0455819242cce7fd0e1
MRQ67 <kasaboody@gmail.com>
2025-04-29 10:21:37 +0300

    Fixing eslint for deployment

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,6 +1,7 @@
 import { dirname } from "path";
 import { fileURLToPath } from "url";
 import { FlatCompat } from "@eslint/eslintrc";
+import { defineConfig } from "eslint/config";
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
@@ -9,13 +10,11 @@ const compat = new FlatCompat({
   baseDirectory: __dirname,
 });
 
-const eslintConfig = [
+export default defineConfig([
   {
-    "rules": {
-      "@typescript-eslint/no-explicit-any": "off"
-    }
+    rules: {
+      '@typescript-eslint/no-explicit-any': 'off',
+    },
   },
   ...compat.extends("next/core-web-vitals", "next/typescript"),
-];
-
-export default eslintConfig;
+]);
